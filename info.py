618cc5a9ccca0b36f72cf76c6a6c694529929733
Mzseel <j3692905@gmail.com>
2024-07-14 22:54:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -109,8 +109,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://kind-mole-j3692905-e1352c7e.koyeb.app".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://kind-mole-j3692905-e1352c7e.koyeb.app".format(FQDN, PORT)
+URL = "kind-mole-j3692905-e1352c7e.koyeb.app".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "kind-mole-j3692905-e1352c7e.koyeb.app".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -125,6 +125,6 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https:/kind-mole-j3692905-e1352c7e.koyeb.app".format(FQDN)
+    URL = "kind-mole-j3692905-e1352c7e.koyeb.app".format(FQDN)
 else:
-    URL = "https://kind-mole-j3692905-e1352c7e.koyeb.app".format(FQDN)
+    URL = "kind-mole-j3692905-e1352c7e.koyeb.app".format(FQDN)
