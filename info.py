411c127cae567995301e439218d1ad6804a64e4c
Thenameisgabbar <159482560+Thenameisgabbar@users.noreply.github.com>
2024-08-04 22:57:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -109,8 +109,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "conscious-isahella-mzmedua.koyeb.app".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "conscious-isahella-mzmedua.koyeb.app".format(FQDN, PORT)
+URL = "https://conscious-isahella-mzmedua.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://conscious-isahella-mzmedua.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
